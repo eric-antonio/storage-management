49d0b5a819cb318ac8c4bcd58898ec9c7c497dfd
Éric  António <ericantoniodev@gmail.com>
2025-06-17 10:57:40 +0200

    auth

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/components/AuthForm";
+import React from "react";
+
+const SifnUp = () => <AuthForm type="sign-up" />;
+
+export default SifnUp;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex-center">
+      <h1 className="text-3xl text-brand">
+        StoreIt The only store Solution you need
+      </h1>
+    </div>
+  );
+}
