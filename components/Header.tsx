c670b6cdb4665e3f46c6b58a7d125bff77c6c4d7
Éric  António <ericantoniodev@gmail.com>
2025-06-20 11:24:32 +0200

    feat:Sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader />
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="Sign Out"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,79 @@
+"use client";
+import { avatarPlaceholderUrl, navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const pathname = usePathname();
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full-brand.svg"
+          alt="Logo"
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="Logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map((item) => (
+            <Link href={item.url} key={item.name} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === item.url && "shad-active"
+                )}
+              >
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === item.url && "nav-icon-active"
+                  )}
+                />
+                <p className="hidden lg:block">{item.name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <Image
+        src="/assets/images/files-2.png"
+        alt="logo"
+        width={506}
+        height={418}
+        className="w-full"
+      />
+      <div className="sidebar-user-info">
+        <Image
+          src={avatarPlaceholderUrl}
+          alt="Avater"
+          width={44}
+          height={44}
+          className="sidebar-user-avatar"
+        />
+        <div className="hidden lg:block">
+          <p className="subtitle-2">Full Name</p>
+          <p className="caption">email</p>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
