4acbbf320599e70ca61ce50c8b6cc46a97b47348
Éric  António <ericantoniodev@gmail.com>
2025-06-28 17:03:28 +0200

    feat:Save Progress Share Files

--- components/ShareInput.tsx
+++ components/ShareInput.tsx
@@ -2,7 +2,7 @@ import { Models } from "node-appwrite";
 import React from "react";
 
 const ShareInput = ({ file }: { file: Models.Document }) => {
-  return <div>ShareInput</div>;
+  return <div><p></p></div>;
 };
 
 export default ShareInput;
