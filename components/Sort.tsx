c1d1f56d86d9b474fd39c1e748a3d0d50017e6ba
Éric  António <ericantoniodev@gmail.com>
2025-07-01 13:05:22 +0200

    feat:Sort page

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,7 +1,38 @@
+"use client";
 import React from "react";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
 const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const path = usePathname();
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content bg-slate-100">
+        {sortTypes.map((sortType) => (
+          <SelectItem
+            className="shad-selecr-item"
+            key={sortType.value}
+            value={sortType.value}
+          >
+            {sortType.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Sort;
